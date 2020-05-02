ca49f4449971c93b4b3cc8c9a67a3ea950e01d73
Glitch (hello-webpage) <none>
2020-05-02 18:35:13 +0000

    🛳🌯 Checkpoint ./script.js:12470601/410:12487641/11 ./application.html:12470601/361

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var final = false;
 
 var markerLetterU = document.getElementById("markerLetterU");
+var markerLetterR = document.getElementById("markerLetterR");
+
 var markerLetterS = document.getElementById("markerLetterS");
+var markerLetterO = document.getElementById("markerLetterO");
+
 
 
 var palavra = [];
@@ -9,8 +13,6 @@ var palavra = [];
 var verificarLetra = (letra) => {
   // Essa função é chamada para verificar
   // se já existe algum letra no array "palavra"
-  
-  this.
   
   var existe = false;
   palavra.forEach(item => { 
@@ -31,11 +33,15 @@ markerLetterU.addEventListener('markerFound', function(){
   verificarLetra("U");
 });
 
+markerLetterR.addEventListener('markerFound', function(){
+  verificarLetra("R");
+});
+
 markerLetterS.addEventListener('markerFound', function(){
   verificarLetra("S");
 });
 
-markerLetterU.addEventListener('markerLost', function(){
-  palavra.filter( e => e !== 'U')
+markerLetterO.addEventListener('markerFound', function(){
+  verificarLetra("O");
 });
 

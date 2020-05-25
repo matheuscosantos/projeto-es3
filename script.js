fb3e5ba2f7eb1f2f438d1ec0010c9232447e03f6
Glitch (hello-webpage) <none>
2020-05-25 16:34:47 +0000

    🍉👨‍👨‍👦 Checkpoint ./script.js:12487641/89

--- script.js
+++ script.js
@@ -85,5 +85,5 @@ markerLetterO.addEventListener('markerFound', function(){
 
 function addLetter(letter) {
  let divPalavra = document.getElementById('teste');
-  
+ var h1Letra = document.createElement('h1'); 
 }

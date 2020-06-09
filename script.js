272db2ebfc99dae02851df6064946ad9a0591dc7
Glitch (hello-webpage) <none>
2020-06-09 14:26:19 +0000

    🔖☔️ Checkpoint ./script.js:12487641/124 ./application.html:12487641/19 ./teste.js:12487641/192

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ var markerLetterO = document.getElementById("markerLetterO");
 var palavra = [];
 var palavraUrso = ["U","R","S","O"];
 
-
-
-function addLetter(letter) {
-  let letraHtml = document.getElementById(letter);
-  letraHtml.style.color = 'red';
-}
-
 var verificarLetra = (letra) => {
   // Essa função é chamada para verificar
   // se já existe algum letra no array "palavra"
---
+++ teste.js
@@ -0,0 +1,6 @@
+function addLetter(letter) {
+  let letraHtml = document.getElementById(letter);
+  letraHtml.style.color = 'red';
+}
+
+export default addLetter;
